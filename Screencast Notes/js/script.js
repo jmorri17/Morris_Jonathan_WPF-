fdda86fa1956b13080bcd5be18ab009cc6f482e5
jmorri17 <jmorri17@fullsail.edu>
2015-03-11 22:05:58 -0500

    Notes
    examples / notes from screencast

---
+++ Screencast Notes/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Created by JAMMBP-3 on 3/11/15.
+ */
+
+
+var x = 5;
+console.log (x++);
+
+//Epressions - Modulo Operator
+
+var remainder = 15 % 2;
+console.log(remainder);
+
+
+//Epressions - Concatenating Strings
+
+var firstName = "Jonathan";
+var lastName = "Morris";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+var a = "6";
+var b = "7";
+
+//var result = a + b;
+//console.log(result);
