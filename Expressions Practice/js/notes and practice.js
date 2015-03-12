8094ea8bd31f7cc1de715523ee70671ca44470fa
jmorri17 <jmorri17@fullsail.edu>
2015-03-11 22:05:01 -0500

    created JS and HTML files for Expressions Assignment

---
+++ Expressions Practice/js/notes and practice.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Jonathan Morris on 3/10/15.
+ */
+;
+
+//alert ("JavaScript works!");
+//Dog Years
+
+//Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”
+// Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+// variables
+// x = dogSparky
+// y = human
+
+var x =(1);
+var y =(7);
+//alert(x * y);
+
+//output
+
+console.log ('Sparky is' + (x * y));
+
+//Slice of Pie part1
+
