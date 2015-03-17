58af094f66b2b9a402345b3c88061c9779f1811f
jmorri17 <jmorri17@fullsail.edu>
2015-03-17 13:38:52 -0500

    Week 2 Flowchart

--- Morris_Jonathan_Conditionals_Assignment/js/script.js
+++ Morris_Jonathan_Conditionals_Assignment/js/script.js
@@ -15,7 +15,7 @@ var friday = false;//this variable holds the value of whether it is friday or no
 
 //if today is payday and if i get paid 1600 dollars i can buy the guitar, if not i have to wait
 
-if(payDay == true && payCheck >= 1600) /*if today is payday, and if my check is greater than or equal to 1600*/{
+if(getPaid == true && payCheck >= 1600) /*if today is payday, and if my check is greater than or equal to 1600*/{
     console.log("Lets go buy a Les Paul!");//if the above conditions correct print Lets go buy a Les Paul"
 }else if(friday = true || payCheck < 1600)/*If today is friday, then it is payday or if my paycheck is less than 1600*/{
     console.log("Looks like you have to wait a bit longer");//if the above conditions are met, print "Looks like you have to wait a bit longer"
