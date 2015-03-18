b8722ab8590646291fc7ef3d8ee9bc80427b96cb
jmorri17 <jmorri17@fullsail.edu>
2015-03-18 10:59:00 -0500

    created JS and HTML files for Expressions Assignment

--- Morris_Jonathan_Conditionals_Assignment/js/script.js
+++ Morris_Jonathan_Conditionals_Assignment/js/script.js
@@ -4,30 +4,21 @@
 //Add alert to test the file
 //alert ("JavaScript works!");
 
+//variables
+var payYes = "Yes"; //Today is payday
+var payNo = "No"; //It is not payday :(
+var ask = prompt("Do you want a new guitar today?\nYes or\nNo"); //Ask if you want to go buy a new guitar
 
-//Buying a Guitar/Les Paul
+//Conditionals else if and output
 
-var savings = 1500;//this variable holds the value of how much i have in my savings account
-var LPcost = 3000;//this variable is how much the Les Paul costs
-var payCheck = prompt("Is your paycheck 1600 or more?\nYes or \nNo");//this variable will ask if your paycheck amount is the avg amount
-var friday =  prompt("Is it Friday?\nYes or\nNo");//this variable will ask if it is friday or not
-var getPaid = friday;//this variable is when it is payday
-
-//if today is payday and if i get paid 1600 dollars i can buy the guitar, if not i have to wait
-
-if(getPaid == false && payCheck >= 1600) /*if today is payday, and if my check is greater than or equal to 1600*/{
-    console.log("Lets go buy a Les Paul!");//if the above conditions correct print Lets go buy a Les Paul"
-}else if (!(friday = true || payCheck < 1600)) {
-    console.log("Sorry Dude! You can't buy a guitar today");//if both conditions are met or true, print "Sorry Dude! You can't buy a guitar today"
-} else {
-    console.log("Lets go buy a Les Paul!!");//if the above conditions are not met, print "Looks like you have to wait a bit longer"
+if(ask == payYes){
+    alert("It is Payday!/ Lets go buy your new guitar!")
+    console.log("User says that it is payday, Lets go buy a guitar! "); //If answered Yes it is payday
+}else if(ask == payNo){
+    alert("Sorry, it does not look like you can buy a guitar today! ")
+    console.log("User says today is not Payday, No guitar for you today :( "); //Answered No it is not payday
+}if(ask == payYes){
+    var whatKind = prompt("Are you going to buy a Les Paul? ");
+}else if(ask == payNo){
+    console.log("Dude that sucks! ");
 }
-
-//outputs
-console.log("My savings account balance is " + savings ,".");
-console.log("The Les Paul I want cost " + LPcost + ".");
-console.log("Is it Friday?" + friday);
-console.log("My paycheck amount is " + payCheck);
-
-
-//Scraping this cannot get it to calculate correctly, back to the drawing board.
