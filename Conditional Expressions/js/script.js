2267cf26f60ec67458be85e87b212c28b99490e9
jmorri17 <jmorri17@fullsail.edu>
2015-03-18 11:40:57 -0500

    Conditionals
    Week 3 Screencast

---
+++ Conditional Expressions/js/script.js
@@ -0,0 +1,16 @@
+//Jonathan Morris
+//SDI - Week 3 Conditional Expression Screencast
+
+var kidsHeight = 40;
+var minHeight  = 48;
+
+//if the child is old enough, print to the console.log "you can ride!"
+//if the kid is over 48 inches in height
+
+if (minHeight < kidsHeight + 2) {
+    //code performed if condition is true
+    console.log("You can ride the Coaster!");
+} else {
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do first!");
+}
