a29f6db60aa61edb6bbcbab14c9905cf0b56b562
jmorri17 <jmorri17@fullsail.edu>
2015-03-18 22:12:20 -0500

    created JS and HTML files for Expressions Assignment

--- Morris_Jonathan_Conditionals_Assignment/js/script.js
+++ Morris_Jonathan_Conditionals_Assignment/js/script.js
@@ -4,21 +4,21 @@
 //Add alert to test the file
 //alert ("JavaScript works!");
 
-//variables
 var payYes = "Yes"; //Today is payday
 var payNo = "No"; //It is not payday :(
-var ask = prompt("Do you want a new guitar today?\nYes or\nNo"); //Ask if you want to go buy a new guitar
 
-//Conditionals else if and output
+var ask1 = prompt("Is it payday?\nYes or \nNo");
+var ask2 = prompt("Do you want a new guitar today?\nYes or\nNo"); //Ask if you want to go buy a new guitar
 
-if(ask == payYes){
-    alert("It is Payday!/ Lets go buy your new guitar!")
+if(ask1 == payYes){
+    alert("It is Payday!/ Lets go buy your new guitar!");
     console.log("User says that it is payday, Lets go buy a guitar! "); //If answered Yes it is payday
-}else if(ask == payNo){
-    alert("Sorry, it does not look like you can buy a guitar today! ")
+}else if(ask1 == payNo){
+    alert("Sorry, it does not look like you can buy a guitar today! ");//If answered No
     console.log("User says today is not Payday, No guitar for you today :( "); //Answered No it is not payday
-}if(ask == payYes){
-    var whatKind = prompt("Are you going to buy a Les Paul? ");
-}else if(ask == payNo){
+}if(ask2 == payYes){
+    alert("Sweet I am buying a new guitar!" );
+}else if(ask2 == payNo){
+    alert("Are you sure? ");
     console.log("Dude that sucks! ");
 }
