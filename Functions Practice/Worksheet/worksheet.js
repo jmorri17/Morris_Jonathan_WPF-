b78e27f464bcfd64a2b5cf1c99c555971e3e7ca9
jmorri17 <jmorri17@fullsail.edu>
2015-03-26 13:10:26 -0500

    Functions

---
+++ Functions Practice/Worksheet/worksheet.js
@@ -0,0 +1,26 @@
+//Worksheet
+
+//Problem: Calculate the area of a rectangle given the width and height of the rectangle
+
+var radius = 5;// radius of the circle
+var circomference = (2 * 3 * radius);
+console.log ("The circumfrence of you circle is + circumference + sq ft")
+
+function circom(){
+    return 2 * 3 * radius
+}
+
+//Stung!
+
+//It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
+
+var stingers = 8.666666667;
+var weight = 200;
+var Kill = (stingers * weight);
+console.log ("It takes " + Kill + " bees to kill an animal that weighs ")
+
+function Stingers(stingers * weight){
+    return stingers * weight
+}
+
+
