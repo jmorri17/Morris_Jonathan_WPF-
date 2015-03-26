36ddb3ccadc55c244e3e984edbbbc820d3bb4300
jmorri17 <jmorri17@fullsail.edu>
2015-03-26 10:44:29 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml

--- Morris_Jonathan_Functional_Assignment/js/script.js
+++ Morris_Jonathan_Functional_Assignment/js/script.js
@@ -9,7 +9,7 @@ var userInput =prompt("What game would you like to play? \n\n Florida Lottery or
 var flLotto; //lotto variable
 var pwrball; // powerball variable
 
-function gameValidation {
+function gameValidation(choice) {
     var timesClicked = 1;
 
     while (choice === "") {
@@ -20,21 +20,26 @@ function gameValidation {
             console.log("Please make a proper selection, Powerball or Fl Lotto?");
             break;  //ends loop after 10 clicks
         }
-        return userInput
+        return choice; //userInput
     }
 userInput = gameValidation;
 
 //Florida Lotto
 function randomNumGen(min, max, num) {
+
     var newArray = []; //new array
-    for (var i = 0; i < num; i++) { // loop for numbers generated
 
-        var flLotto = Math.round(Math.random() * (max - min) + min);
+    for (var i = 0; i < num; i++) {
 
+        var myRandomNumber = Math.round(Math.random() * (max - min) + min);
+
+        newArray[i] = myRandomNumber;
     }
     return newArray;
 }
-flLotto = randomNumGen(1, 90, 5); // generate 5 numbers between 1 and 90
+
+// generate 5 numbers between 1 and 90
+flLotto = randomNumGen(1, 90, 5); }
 
 
 
