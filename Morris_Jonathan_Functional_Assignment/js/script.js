80d00cda276d4b9c3883e51130af88dc85b87093
jmorri17 <jmorri17@fullsail.edu>
2015-03-26 22:48:01 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml

--- Morris_Jonathan_Functional_Assignment/js/script.js
+++ Morris_Jonathan_Functional_Assignment/js/script.js
@@ -5,29 +5,31 @@
 //alert ("JavaScript works!");
 
 //variables
-var userInput =prompt("What game would you like to play? \n\n Florida Lottery or Powerball?"); //prompt for lottery type
-var flLotto; //lotto variable
-var pwrBall; // powerball variable
+//var userInput;//prompt for lottery type
+//var flLotto; //lotto variable
+//var pwrBall; // powerball variable
 
 //functions
 
-function gameValidation (choice) {
+function gameValidation () {
     var timesClicked = 1;
+    var choice = prompt("What lottery type would you like to play?");
+    while ((choice != "Florida") && (choice != "Powerball")) { //While variable choice is not equal to string Florida and variable not equal to string Powerball ask again
+    choice = prompt("I'm sorry I did not get your selection, Please try again");
 
-    while (choice === "((choice !== 1) and (choice !== 2)") {
-
-        userInput = prompt("Do not leave this blank.\n\nPlease type in a selection:");
-
-        timesClicked++;
-
-        if (timesCLicked === 10) { // if no entry is made after 10 clicks then user will receive a message to make a selection
-
-            console.log('Please make a proper selection, Powerball or Florida Lottery ?');
-
-            break;  //ends loop after 10 clicks
-        }
     }
+
     userInput = gameValidation;
+    if(choice == "Florida"){
+
+        var magicNumbers = randomNumGen(1,99,5);
+        console.log ("Your Florida winning Lotto numbers are! " + magicNumbers.join(", "));
+    } else if(choice == "Powerball"){
+
+        var magicNumbers = randomNumGen(1,99,5);
+        var powerBall = randomNumGen(1,99,1);
+        console.log ("Your Powerball winning Lotto numbers are! " + magicNumbers.join(", ") + " Powerball is " + powerBall);
+    }
 }
 //Florida Lotto Function
 function randomNumGen(min, max, num) {
@@ -36,35 +38,11 @@ function randomNumGen(min, max, num) {
 
     for (var i = 0; i < num; i++) {
 
-        var myRandomNumber = Math.round(Math.random() * (max - min) + min);
+        RandomNumberPB = Math.round(Math.random() * (max - min) + min);
 
-        newArray[i] = myRandomNumber;
+        newArray[i] = RandomNumberPB;
     }
     return newArray;
 }
 
-// generate 5 numbers between 1 and 90
-flLotto = randomNumGen(1, 90, 5);
-
-//Powerball Function
-
-function randomNumGenPb(min, max, num) {
-    var newArray1 = []; //new array
-
-    for (var i = 0; i < num; i++) {
-
-        var myRandomNumberPB = Math.round(Math.random() * (max - min) + min);
-
-        newArray1[i] = myRandomNumberPB;
-    }
-    return newArray1;
-}
-// generate 6 numbers for powerball
-pwrball = randomNumGen(1, 90, 6);
-
-//main code
-//output
-console.log("You have chosen to play " +gameValidation()); //displays users lottery type choice
-
-(gameValidation === "Florida") ? console.log("Florida lotto numbers are... " + flLotto+ ".") : console.log("The Powerball numbers are... " + pwrBall  + ".");
-
+gameValidation();
